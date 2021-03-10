6e6f9b463079a85a455bdfb921a47d02a2728c21
Scorpion <v.cherner1991@gmail.com>
2021-03-10 21:51:57 +0300

    add simple1.php

---
+++ php script/simple1.php
@@ -0,0 +1,28 @@
+<?php
+$cont = 6;
+$i = 1;
+if ($i<=$cont){
+echo "#"."\n";
+$i++;
+	}
+	if ($i<=$cont){
+echo "##"."\n";
+	$i++;
+	}
+	if ($i<=$cont){
+echo "###"."\n";
+	$i++;
+	}
+	if ($i<=$cont){
+echo "####"."\n";
+	$i++;
+	}
+	if ($i<=$cont){
+echo "#####"."\n";
+	$i++;
+	}
+	if ($i<=$cont){
+echo "######"."\n";
+	$i++;
+	}
+	
