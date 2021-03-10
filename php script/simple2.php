d592d9b3cc069dfcc3266968dd5bbd973e8a447c
Scorpion <v.cherner1991@gmail.com>
2021-03-10 18:46:36 +0300

    initial commit

---
+++ php script/simple2.php
@@ -0,0 +1,7 @@
+<?php
+$arr = [1,3,5,7,9];
+$max = array_sum($arr)-min($arr);
+$min = array_sum($arr)-max($arr); 
+echo "Максимальное: "."$max"."\n";
+echo "Минимальное: "."$min"."\n";
+echo "{｡^◕‿◕^｡} "."\n";
