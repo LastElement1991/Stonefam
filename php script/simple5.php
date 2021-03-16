601711d9a840bbe43a7316c5d33d33cb3457623b
Scorpion <v.cherner1991@gmail.com>
2021-03-16 13:29:13 +0300

    edit simple5.php

--- php script/simple5.php
+++ php script/simple5.php
@@ -4,16 +4,16 @@ function srt_name(string $name, int $length_name): string
 {
     $str = '';
     $i = 0;
-    while ($i < $length_name) {
-        $str .= $name[$i];
-        $i++;
-        if ($name[$i] == '') {
-            while ($i < $length_name) {
-                $str .= ' ';
+        while ($i < $length_name) {
+                $str .= $name[$i];
                 $i++;
+                if ($name[$i] == '') {
+                    while ($i < $length_name) {
+                        $str .= ' ';
+                        $i++;
+                    }
+                }
             }
-        }
-    }
     return $str;
 }
 
@@ -35,7 +35,7 @@ function srt_air($air, $length_air): string
 
 //echo "Name - ";
 //$name=readline('');
-$name = 'Вадимка';
+$name = 'Вадимкавфцфц';
 $name = srt_name($name, 6);
 //echo "Surname - ";
 //$surn=readline('');
@@ -43,11 +43,12 @@ $surn = 'Vadimkaaaaa';
 $surn = srt_name($surn, 6);
 //echo "Airport departure - ";
 //$dep=readline('');
+$dep = 'AER';
 $dep = srt_air($dep, 3);
 //echo "Airport arrival - ";
 //$arr = readline('');
+$arr = 'MOW';
 $arr = srt_air($arr, 3);
-print_r($name . "/////////////////");
 echo "
  ╔═════════════╦══════════════╦══════════════════════╗
  ║ Фамилия     ║ $surn       ║                      ║
