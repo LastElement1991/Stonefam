53a2d171c5d8323984b7eea3058c90f612457f77
Scorpion <v.cherner1991@gmail.com>
2021-03-17 16:11:50 +0300

    add php 903 and 905

--- php script/903.php
+++ php script/903.php
@@ -1,13 +1,38 @@
 <?php
-function max ($one)
-{
-    $str='';
-    $i = 0;
+//function max_element ($one)
+//{
+    $one = 328;
+    $int = "$one";
+      $i = 0;
+    $str = '';
+//    if ($i > $str) {
+//        $str = $int[$i];
+//        $i++;
+//    }
+if ($int[$i] > $str) {
+    $str = $int[$i];
+    $i++;
+
 }
-while ($i < $lenght) {
-    $str_air .= $name[$i];
+$str3=$int[$i];
+    if ($str3<=$str) {
+        $str = $int[$i];
+        $i++;
+    }
+$str3=$int[$i];
+if ($str3<=$str) {
+    $str = $int[$i];
     $i++;
 }
-$int_1 = 328;
-$int_2 = 832;
+    print_r($str);
+//    return str;
+//}
 
+
+$int_1 = 328;
+//$result = max_element($int_1);
+//print_r($result);
+$str ="$int_1";
+$str = "$int_1";
+$int_2 = 832;
+$str_2= "$int_2";
