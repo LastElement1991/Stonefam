13185fb89779d6375cae2d7135a65027ff78309b
Scorpion <v.cherner1991@gmail.com>
2021-05-27 18:01:43 +0300

    corect file

--- php script/program.php
+++ php script/program.php
@@ -1,51 +1,48 @@
 <?php
-// echo "1. Login\n";
-// echo "2. Registration\n";
-// echo "3. Exit\n";
-// $x = readline();
+echo "1. Login\n";
+echo "2. Registration\n";
+echo "3. Exit\n";
+$x = readline();
 
-// system('clear');
-// if ($x == '1')
-//  {
-// echo "Please enter login\n";
-// $login = readline();
-// echo "Please enter password\n";
-// $password = readline();
-//  	$mass = file_get_contents('users.json');
-// $info = json_decode($mass);
-// echo $info;
-// die;
-// if( $mass!= false && !is_null($info)){
-//     foreach($info as $k => $e){
-//         echo '<p>'  . $e . '</p>';
-//     }
-// }
-//  }
+system('clear');
+if ($x == '1')
+ {
+echo "Please enter login\n";
+$login = readline();
+echo "Please enter password\n";
+$password = readline();
+ 	$mass = file_get_contents('users.json');
+$info = json_encode($mass);
+$mass = json_decode($info);
+print_r($mass);
+if( $mass!= false){
+    foreach($info as $result ){
+        echo  $result ;
+    }
+}
+ }
+ elseif ($x == '2'){
+ echo "Please enter login\n";
+$login = readline();
+echo "Please enter password\n";
+$password = readline();
+$str = ',{
+   "login": "'.$login.'",
+   "password": "'.$password.'",
+   "role": "user"
+ }
+';
+file_put_contents('users.json',$str, FILE_APPEND | LOCK_EX );
+system('clear');
+ }
 
-//  elseif ($x == '2'){
-//  echo "Please enter login\n";
-// $login = readline();
-// echo "Please enter password\n";
-// $password = readline();
+else {
+system('exit');	
+}
 
-
-
-// $str = ',{
-//    "login": "'.$login.'",
-//    "password": "'.$password.'",
-//    "role": "user"
-//  }
-// ';
-// file_put_contents('users.json',$str, FILE_APPEND | LOCK_EX );
-// system('clear');
-//  }
-
-// else {
-// system('exit');	
-// }
 $mass = file_get_contents('users.json', true);
-$info = explode('"login": "', $mass);
- foreach($info as $k => $e){
-print_r($e);
+$info = explode("\n", $mass);
+ foreach($info as $result ){
+print_r($result);
 die;
    }
