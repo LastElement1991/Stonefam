bc61e30125da5fd4fc83b2ec53b61f30bf850cf8
Scorpion <v.cherner1991@gmail.com>
2021-05-28 10:45:57 +0300

    correct script

--- php script/program.php
+++ php script/program.php
@@ -13,16 +13,15 @@ echo "Please enter password\n";
 $password = readline();
  $result = file_get_contents('users.json');
 $json = json_decode($result, true);
-//foreach($json as $result ){
-  //      echo  $result ;
-
-if( $json = ['login'] !== $login ){
-    echo "Пользователь не найден в системе";
+foreach($json as $result ){
+if( $result = ['login'] !== $login ){
+    echo "Пользователь не найден в системе"."\n";
 
     }
 
+}
  }
- elseif ($x == '2'){
+ else if ($x == '2'){
  echo "Please enter login\n";
 $login = readline();
 echo "Please enter password\n";
@@ -33,8 +32,11 @@ $mass =  array (
 'login' => $login,
 'password' => $password
 );
-$json = json_encode($mass);
-file_put_contents('users.json',$json, FILE_APPEND | LOCK_EX );
+$json = json_encode($mass,JSON_PRETTY_PRINT);
+$json = ",".$json."]";
+$str = file_get_contents('users.json');
+$json = str_replace("]", $json, $str);
+file_put_contents('users.json',$json);
 system('clear');
  }
 
